75f639f55ae62ad53685f0aa71c1022d576ac027
Marcelo Henrique <marcelohphn@gmail.com>
2021-06-23 12:25:26 -0300

    Criação da pagina NewRoom (criar novas salas)

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -4,8 +4,7 @@ import googleIconImg from '../assets/images/google-icon.svg';
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
 
-
-export function Home()
+export function NewRoom()
 {
     return (
         <div id="page-auth">
@@ -17,20 +16,18 @@ export function Home()
             <main>
                 <div className="main-content">
                     <img src={logoimg} alt="Letmeask" />
-                    <button className="create-room">
-                        <img src={googleIconImg} alt="Logo do Google" />
-                        Crie sua sala com o Google
-                    </button>
-                    <div className="separator">Ou entre em uma sala</div>
+                    <h2>Criar uma nova sala</h2>
+                    
                     <form>
                         <input
                          type="text"
-                         placeholder = "Digite o codigo da sala"
+                         placeholder = "Nome da sala"
                         />
                         <Button type="submit">
-                          Entrar na sala
+                          Criar sala
                         </Button>
                     </form>
+                    <p>Quer entrar em uma sala existente? <a href="#">Clique aqui</a></p>
                 </div>
             </main>
         </div>
