1869ef30419205ce072d20a7a95f20c17d3a4f4b
Marcelo Henrique <marcelohphn@gmail.com>
2021-06-24 21:25:08 -0300

    Implementando criação de sala

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,15 +1,38 @@
-import {Link} from 'react-router-dom';
+import { FormEvent, useState} from 'react';
+import {Link, useHistory} from 'react-router-dom';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoimg from '../assets/images/logo.svg';
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
+import { database } from '../services/firebase';
+import { useAuth } from '../hooks/useAuth';
 /*  import { useAuth } from '../hooks/useAuth';  */ 
 
 
 export function NewRoom()
 {
-
      /*  const {user} = useAuth();  */
+     const {user} = useAuth();
+     const history = useHistory();
+     const [newRoom, setNewRoom] = useState('');
+
+     async function hundleCreateRoom(event: FormEvent){
+       event.preventDefault();
+       
+       if(newRoom.trim() === ''){
+           return;
+       }
+
+       const roomRef = database.ref('rooms');
+
+       const firebaseRoom = await roomRef.push({
+           title: newRoom,
+           authorId: user?.id,
+
+       })
+
+       history.push(`/rooms/${firebaseRoom.key}`)
+     }
     
     return (
         <div id="page-auth">
@@ -25,10 +48,12 @@ export function NewRoom()
                     
                     <h2>Criar uma nova sala</h2>
                     
-                    <form>
+                    <form onSubmit={hundleCreateRoom}>
                         <input
                          type="text"
                          placeholder = "Nome da sala"
+                         onChange = {event => setNewRoom(event.target.value)}
+                         value = {newRoom}
                         />
                         <Button type="submit">
                           Criar sala
